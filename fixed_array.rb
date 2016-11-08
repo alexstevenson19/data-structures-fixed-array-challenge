f6dadff4d64c8268918061c72122e88cce08efbf
Alex Stevenson <alexstevenson19@gmail.com>
2016-11-07 23:04:52 -0600

    initial commit, write first rspec test, and initialization to pass it.

--- fixed_array.rb
+++ fixed_array.rb
@@ -1,3 +1,9 @@
 class FixedArray
 
+	attr_reader :array
+
+	def initialize(size)
+		@array = Array.new(size)
+	end
+
 end
--- fixed_array_spec.rb
+++ fixed_array_spec.rb
@@ -1,5 +1,13 @@
 require_relative 'fixed_array'
 
 describe FixedArray do
+	let(:new_array) { FixedArray.new(3) }
+
+	describe "fixed array size" do
+		it "should have a set size" do
+			p new_array.array.length
+			expect(new_array.array.length).to eq(3)
+		end
+	end
   
 end
