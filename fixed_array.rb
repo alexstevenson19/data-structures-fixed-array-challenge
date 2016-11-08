2f497cd5c936ffc61440a10b927e8002a4f90331
Alex Stevenson <alexstevenson19@gmail.com>
2016-11-08 11:37:17 -0600

    add set and get methods for fixed array, and write an extra test demonstrating the difference between rspec's .to eq and .to be (with 'be' returning the eact same object in memory, not just the same string)

--- fixed_array.rb
+++ fixed_array.rb
@@ -2,8 +2,26 @@ class FixedArray
 
 	attr_reader :array
 
-	def initialize(size, content)
-		@array = Array.new(size, content)
+	def initialize(size)
+		@array = Array.new(size)
+	end
+
+	def set(indx, value)
+		index_limit = @array.length - 1
+		if indx > index_limit
+		 #throw error
+	 else
+	 	@array[indx] = value
+	 end 
+	end
+
+	def get(indx)
+		index_limit = @array.length - 1
+		if indx > index_limit
+		 #throw error
+	 else
+	 	@array[indx]
+	 end 
 	end
 
 end
--- fixed_array_spec.rb
+++ fixed_array_spec.rb
@@ -1,7 +1,7 @@
 require_relative 'fixed_array'
 
 describe FixedArray do
-	let(:new_array) { FixedArray.new(3, "hi") }
+	let(:new_array) { FixedArray.new(3) }
 
 	describe "fixed array size" do
 		it "should have a set length" do
@@ -12,6 +12,25 @@ describe FixedArray do
 		it "should not be a different length" do
 			expect(new_array.array.length).not_to eq(4)
 		end
+	end
+
+	describe "fixed array value at index" do
+		it "returns an element of a given index" do
+			new_array.set(1, "hello")
+			# new_array[2] = "goodbye"
+			p "-------------------------------"
+			p new_array
+			expect(new_array.get(1)).to eq "hello"
+		end
+
+		#write a test for .to be, which needs the exact object
+		it "returns the same object at a given index" do
+			greeting = "goodbye"
+			new_array.set(2, greeting)
+			expect(new_array.get(2)).to be greeting
+		end
+
+		
 	end
   
 end
