a14a07bacc5a5352e00a7713d42c217ddeb14a3a
Alex Stevenson <alexstevenson19@gmail.com>
2016-11-07 23:07:45 -0600

    add second (not equal) rspec test)

--- fixed_array_spec.rb
+++ fixed_array_spec.rb
@@ -4,9 +4,13 @@ describe FixedArray do
 	let(:new_array) { FixedArray.new(3) }
 
 	describe "fixed array size" do
-		it "should have a set size" do
-			p new_array.array.length
+		it "should have a set length" do
+			# p new_array.array.length
 			expect(new_array.array.length).to eq(3)
+		end
+
+		it "should not be a different length" do
+			expect(new_array.array.length).not_to eq(4)
 		end
 	end
   
